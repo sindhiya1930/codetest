13129b54f7352cac49f9518ecd1c74394619a222
sindhiya1930 <33276851+sindhiya1930@users.noreply.github.com>
2019-08-07 13:06:13 +0530

    Update codetest.groovy

--- codetest.groovy
+++ codetest.groovy
@@ -181,7 +181,7 @@ pipeline{
                     sleep 140
                     #post deployment verification and rollback
                     #post deployment script for dev_deploymentaddress.yml file
-                    curl -X GET --header 'Accept: application/json' --header '${PARAMETERS}' '${URL}' -u $USERNAME:$PASSWORD > result.txt
+                    curl -X GET --header 'Accept: application/json' --header ${PARAMETERS} ${URL} -u $USERNAME:$PASSWORD > result.txt
                     status=`grep -E "Bad Request|Server Error" result.txt| wc -l`
                     echo $status
                     if [ $status -eq 0 ]; then
