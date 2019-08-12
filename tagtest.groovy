182c426ae043b9b675342ff68a157802374c49e7
sindhiya1930 <33276851+sindhiya1930@users.noreply.github.com>
2019-08-12 10:21:53 +0530

    Update tagtest.groovy

--- tagtest.groovy
+++ tagtest.groovy
@@ -8,7 +8,6 @@ pipeline{
     agent any
            environment {
                SERVICE_NAME = sh(script: "git describe --tags \$(git rev-list --tags --max-count=1)| cut -d'_' -f1", , returnStdout: true).trim()
-		CAT = ""
           }
 	
 	
