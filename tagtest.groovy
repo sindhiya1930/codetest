76f03c94e414c70f361b9290145ebfb0b517ad13
sindhiya1930 <33276851+sindhiya1930@users.noreply.github.com>
2019-08-13 11:08:06 +0530

    Update tagtest.groovy

--- tagtest.groovy
+++ tagtest.groovy
@@ -4,6 +4,9 @@ def getEnvVar(String paramName){
 }
 pipeline{
     agent any
+           environment {
+               SERVICE_NAME = sh(script: "git describe --tags \$(git rev-list --tags --max-count=1)| cut -d'_' -f1", ,returnStdout: true).trim()
+          }
 
 	stages {
    		stage('Git Checkout') { // for display purposes 
